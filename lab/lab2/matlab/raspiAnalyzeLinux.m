3f5b0b4a57fd133540b88c584dd8dac75d8485dc
Gaute Hagen <dudlileif@gmail.com>
2019-01-29 13:27:44 +0100

    Update for lab2

--- lab/lab2/matlab/raspiAnalyzeLinux.m
+++ lab/lab2/matlab/raspiAnalyzeLinux.m
@@ -10,6 +10,7 @@
 
 % Definitions
 channels = 5;   % Number of ADC channels used
+selectedChannels = 3; % First channels
 
 % Open, import and close binary data file produced by Raspberry Pi
 %% FIXME: Change this.
@@ -24,25 +25,26 @@ path = '//run/user/1000/gvfs/smb-share:server=10.22.43.82,share=pi/TTT4280/lab/l
 close all
 fh_raw = figure;    % fig handle
 subplot(2,1,1);
-plot(rawData);
+selectedData=rawData(1:end,1:selectedChannels);
+plot(selectedData);
 ylim([0, 4095]) % 12 bit ADC gives values in range [0, 4095]
 xlabel('sample');
 ylabel('conversion value');
-legendStr = cell(1,channels);
-for i = 1:channels
+legendStr = cell(1,selectedChannels);
+for i = 1:selectedChannels
     legendStr{1,i} = ['ch. ' num2str(i)];
 end
 legend(legendStr,'location','best');
 title('Raw ADC data');
 
 Fs=15330;
-freqres=abs(fft(rawData,Fs));
+freqres=abs(fft(selectedData,Fs));
 subplot(2,1,2);
 semilogx(freqres(2:end,1:end));
 xlabel('frequency');
 ylabel('amplitude');
-legendStr = cell(1,channels);
-for i = 1:channels
+legendStr = cell(1,selectedChannels);
+for i = 1:selectedChannels
     legendStr{1,i} = ['ch. ' num2str(i)];
 end
 legend(legendStr,'location','best');
