2e29cb13339fa667455b1428323c8f1dc9e3f886
Torstein Langan <torsteml@stud.ntnu.no>
2019-03-05 15:29:34 +0100

    Updated

--- lab/lab4/matlab/getPulse.m
+++ lab/lab4/matlab/getPulse.m
@@ -1,6 +1,6 @@
 clear;
-%folderpath = '/Volumes/pi/Videos/lab4/';
-folderpath = '//run/user/1000/gvfs/smb-share:server=10.22.223.81,share=pi/Videos/lab4/';
+folderpath = '/Volumes/pi/Videos/lab4/';
+%folderpath = '//run/user/1000/gvfs/smb-share:server=10.22.223.81,share=pi/Videos/lab4/';
 [file, path] = uigetfile(join([folderpath,'*.mp4']));
 path = join([folderpath,file]);
 [output_channels, sample_rate] = read_video_and_extract_roi(path);
@@ -10,9 +10,10 @@ redChannelHPass = highpass(redChannel,1,sample_rate);
 [peaks,locs] = findpeaks(autocorrelation);
 locsStored = locs-1199;
 locsStored = locsStored(locsStored>=0);
-locsStoredCropped = locsStored(1:end-35);
+%locsStoredCropped = locsStored(1:end-57);
+
 diffPulse = diff(locsStoredCropped);
 pulse = (sample_rate/mean(diffPulse))*60;
-stdPulse = (sample_rate/std(diffPulse))*60;
+stdPulse = std((sample_rate/diffPulse)*60);
 fprintf('Estimated pulse: %.0f\n',pulse);
 fprintf('Estimated standard deviation: %.0f\n',stdPulse);
