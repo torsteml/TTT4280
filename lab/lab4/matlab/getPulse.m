ea604dfbd8a49c8247b51dc07b95ee1364c23cbd
Gaute Hagen <dudlileif@gmail.com>
2019-03-05 15:26:26 +0100

    Updated getPulse

--- lab/lab4/matlab/getPulse.m
+++ lab/lab4/matlab/getPulse.m
@@ -1,9 +1,9 @@
-% clear;
-% % %folderpath = '/Volumes/pi/Videos/lab4/';
+%clear;
+folderpath = '/Volumes/pi/Videos/lab4/';
 % folderpath = '//run/user/1000/gvfs/smb-share:server=10.22.223.81,share=pi/Videos/lab4/';
-% [file, path] = uigetfile(join([folderpath,'*.mp4']));
-%  path = join([path,file]);
-% [output_channels, sample_rate] = read_video_and_extract_roi(path);
+[file, path] = uigetfile(join([folderpath,'*.mp4']));
+path = join([path,file]);
+[output_channels, sample_rate] = read_video_and_extract_roi(path);
 redChannel = output_channels(1:end,1);
 redChannelHPass = highpass(redChannel,1,sample_rate);
 [autocorrelation,lags] = xcorr(redChannelHPass, length(redChannelHPass), 'coeff');
